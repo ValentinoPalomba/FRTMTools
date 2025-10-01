e9b45d3bbba409ab5c23a15295305ec989fa1ee4
Valentino Palomba <valentino.palomba@intesasanpaolo.com>
2025-10-01 17:38:25 +0200

    fix rendition check

--- FRTMTools/Sources/Analyzers/AppAnalyzer/SubAnalyzer/CarAnalyzer.swift
+++ FRTMTools/Sources/Analyzers/AppAnalyzer/SubAnalyzer/CarAnalyzer.swift
@@ -16,23 +16,38 @@ class CarAnalyzer {
         do {
             let reader: Reader<LazyRendition> = try Reader(.init(url))
             let renditions = try reader.read()
-
-            subItems = renditions.compactMap { rendition -> FileInfo? in
+            let filteredRenditions = renditions.reduce([LazyRendition]()) {
+                partialResult,
+                lazyRendition in
+                let isInternal = lazyRendition.renditionClass == "CUIInternalLinkRendition"
+                let isAlredyInWithDifferentRenditionClass = partialResult.contains(
+                    where: {
+                        $0.renditionClass != lazyRendition.renditionClass && $0.renditionName == lazyRendition.renditionName
+                    }
+                )
+                
+                if isInternal {
+                    return partialResult
+                }
+                
+                if isAlredyInWithDifferentRenditionClass {
+                    return partialResult
+                }
+                
+                return partialResult + [lazyRendition]
+            }
+            
+            subItems = filteredRenditions.compactMap { rendition -> FileInfo? in
                 let fileName = rendition.renditionName
                 
                 guard
                     rendition.renditionClass != "CUIInternalLinkRendition",
                     !rendition.isLinkingToPDF,
                     !rendition.isLinkingToSVG,
-                    !fileName.isEmpty,
-                    (rendition.isPDF || rendition.isSVG || rendition.isVector)
+                    !fileName.isEmpty
                 else {
                     return nil
                 }
-                
-                guard rendition.renditionName
-                    .components(separatedBy: ".").count > 1, rendition.fileName
-                    .components(separatedBy: ".").count > 1 else { return nil }
                 
                 let tmpURL = try? rendition.writeTo(.temporaryDirectory)
                 let size = tmpURL.map { $0.allocatedSize() } ?? 0
@@ -40,8 +55,8 @@ class CarAnalyzer {
                 if size == 0 { return nil }
                 
                 return FileInfo(
-                    path: "\(relativePath)/\(fileName)",
-                    name: fileName,
+                    path: "\(relativePath)/\(rendition.fileName)",
+                    name: rendition.fileName,
                     type: .file,
                     size: size,
                     subItems: nil
